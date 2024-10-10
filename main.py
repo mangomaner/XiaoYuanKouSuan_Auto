fd7f8d34511501ea954eaa39ade9a559b518d44c
mangoman <129660503+mangomaner@users.noreply.github.com>
2024-10-10 19:28:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
 import pyautogui as pyautogui
 from cnocr import CnOcr
+
+NUMBER_OF_PROBLEM = 30
 
 ocr = CnOcr(det_model_name='naive_det')
 cur = [1, 2]
@@ -10,15 +11,14 @@ pre = 0
 count = 0
 errorCount = 0
 lock = 0
-NUMBER_OF_PROBLEM = 30
 
 print('ready')
 
 while True:
     try:
         # 识别下一个数字
-        leftPicNext = pyautogui.screenshot(region=(175, 425, 60, 40))   # 左侧的下一个数字
-        rightPicNext = pyautogui.screenshot(region=(320, 425, 60, 40))  # 右侧的下一个数字
+        leftPicNext = pyautogui.screenshot(region=(175, 425, 60, 40))   # 左侧的下一个数字，需修改
+        rightPicNext = pyautogui.screenshot(region=(320, 425, 60, 40))  # 右侧的下一个数字，需修改
         leftPicNext.save('leftNext.png')
         rightPicNext.save('rightNext.png')
         leftOutNext = ocr.ocr_for_single_line('./leftNext.png')
@@ -29,7 +29,7 @@ while True:
         nex[1] = int(rightNext)
 
         # 识别题号
-        jud = pyautogui.screenshot(region=(225, 63, 100, 32))  # 题目区域
+        jud = pyautogui.screenshot(region=(225, 63, 100, 32))  # 题目区域，需修改
         jud.save('jud.png')
         IndexOut = ocr.ocr_for_single_line('./jud.png')
         jud = IndexOut['text']
@@ -37,8 +37,8 @@ while True:
         number = int(jud[0])
 
         if number == 1:
-            leftPic = pyautogui.screenshot(region=(125, 310, 85, 58))  # 左侧的当前数字
-            rightPic = pyautogui.screenshot(region=(315, 310, 85, 58))
+            leftPic = pyautogui.screenshot(region=(125, 310, 85, 58))  # 左侧的当前数字，需修改
+            rightPic = pyautogui.screenshot(region=(315, 310, 85, 58))  # 右侧的当前数字，需修改
             leftPic.save('left.png')
             rightPic.save('right.png')
             leftOut = ocr.ocr_for_single_line('./left.png')
@@ -53,7 +53,7 @@ while True:
         if number != pre:   # 保证每题只画一次
             print(f'第{number}题:', dict[number][0], dict[number][1])
             if dict[number][0] > dict[number][1]:
-                pyautogui.moveTo(229, 675, duration=0)
+                pyautogui.moveTo(229, 675, duration=0)      # 画笔区域，需修改
                 pyautogui.drag(100, 50, 0.1, button='left')
                 pyautogui.drag(-150, 70, 0.1, button='left')
             else:
@@ -67,7 +67,7 @@ while True:
 
         if count > 3:
             if dict[number][0] < dict[number][1]:
-                pyautogui.moveTo(229, 675, duration=0)
+                pyautogui.moveTo(229, 675, duration=0)      # 画笔区域，需修改（同上）
                 pyautogui.drag(100, 50, 0.1, button='left')
                 pyautogui.drag(-150, 70, 0.1, button='left')
             else:
@@ -80,11 +80,11 @@ while True:
     except Exception as e:
         errorCount += 1
         if errorCount > 100 and lock == 1:
-            pyautogui.moveTo(274, 828, duration=2)
+            pyautogui.moveTo(274, 828, duration=2)  # 按钮区域，需修改（开心收下）
             pyautogui.click()
-            pyautogui.moveTo(408, 970, duration=2)
+            pyautogui.moveTo(408, 970, duration=2)  # 按钮区域，需修改（继续）
             pyautogui.click()
-            pyautogui.moveTo(283, 906, duration=2)
+            pyautogui.moveTo(283, 906, duration=2)  # 按钮区域，需修改（再来一局）
             pyautogui.click()
             errorCount = 0
             lock = 0
